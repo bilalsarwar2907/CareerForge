26d0ededeacd8c3a5374701d62e0ff315d1997d8
bilalsarwar2907 <bilalsarwar2907@gmail.com>
2026-08-11 14:08:29 +0200

    chore: remove temp scripts

--- _git_push.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d C:\Users\biges\CareerForge
-git add -A
-git commit -m "Session 2 complete: all 11 checkpoints done, prep for FastAPI Phase 1"
-git push
-echo.
-echo Done! Press any key to close.
-pause
